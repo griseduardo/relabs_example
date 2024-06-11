9457439404d3cf0a80f4f8358fbc908a8765592e
Eduardo Gris <gris.eduardo@gmail.com>
2024-06-11 18:53:58 -0300

    Add non functional login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,373 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HeaderIcons extends StatelessWidget {
+  const HeaderIcons({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return Row(
+      children: [
+        const SizedBox(width: 40),
+        const Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.library_add_rounded,
+                color: Colors.white,
+                size: 40,
+              ),
+            ]
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.cyan.shade50,
+                  width: 2,
+                ),
+              ),
+              child: const CircleAvatar(
+                backgroundImage: AssetImage(
+                  "./lib/assets/brazil.png",
+                ),
+                backgroundColor: Colors.transparent,
+                radius: 30,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Title extends StatelessWidget {
+  const Title({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return const Column(
+      children: [
+        Text(
+          'Login',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Text(
+          'Welcome back!',
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
+
+class FormTextField extends StatelessWidget {
+  const FormTextField({super.key, required this.placeholder});
+  final String placeholder;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TextField(
+        obscureText: placeholder == 'Email' ? false : true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide.none,
+          ),
+          labelText: placeholder,
+          labelStyle: TextStyle(
+            color: Colors.cyan.shade700,
+            fontWeight: FontWeight.w600,
+          ),
+          fillColor: Colors.cyan.shade50,
+          filled: true,
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+    );
+  }
+}
+
+class LoginOptions extends StatelessWidget {
+  const LoginOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Row(
+          children: [
+            RememberCheckbox(),
+            SizedBox(width: 5),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Remember-me',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+              ),
             ),
           ],
         ),
+        Row(
+          children: [
+            TextButton(
+              onPressed: () { },
+              style: TextButton.styleFrom(
+                minimumSize: Size.zero,
+                padding: EdgeInsets.zero,
+              ),
+              child: const Text(
+                'Password Recovery',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ],
+        )
+      ]
+    );
+  }
+}
+
+class RememberCheckbox extends StatelessWidget {
+  const RememberCheckbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 20,
+      width: 20,
+      child: Checkbox(
+        checkColor: Colors.cyan.shade700,
+        fillColor: WidgetStateProperty.all<Color>(Colors.cyan.shade50),
+        value: true,
+        onChanged: (bool? value) { },
+        side: WidgetStateBorderSide.resolveWith(
+              (states) => BorderSide(color: Colors.cyan.shade50),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextButton(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all<Color>(Colors.greenAccent.shade100),
+            padding: WidgetStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.symmetric(vertical: 15)),
+          ),
+          onPressed: () { },
+          child: Text(
+            'Log In',
+            style: TextStyle(
+              color: Colors.green.shade800,
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+      ]
+    );
+  }
+}
+
+class SocialMediaIcons extends StatelessWidget {
+  const SocialMediaIcons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.cyan.shade50,
+          child: FaIcon(
+            FontAwesomeIcons.google,
+            color: Colors.cyan.shade700,
+            size: 16,
+          ),
+        ),
+        const SizedBox(width: 15),
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.cyan.shade50,
+          child: FaIcon(
+            FontAwesomeIcons.xTwitter,
+            color: Colors.cyan.shade700,
+            size: 16,
+          ),
+        ),
+        const SizedBox(width: 15),
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.cyan.shade50,
+          child: FaIcon(
+            FontAwesomeIcons.facebookF,
+            color: Colors.cyan.shade700,
+            size: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LoginDivider extends StatelessWidget {
+  const LoginDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(
+          child: Divider(
+            color: Colors.white,
+          )
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "Or continue with",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            color: Colors.white,
+          )
+        ),
+      ]
+    );
+  }
+}
+
+class SignUpLink extends StatelessWidget {
+  const SignUpLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Doesn't have an account?",
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(width: 2),
+            TextButton(
+              onPressed: () { },
+              style: TextButton.styleFrom(
+                minimumSize: Size.zero,
+                padding: EdgeInsets.zero,
+              ),
+              child: Text(
+                'Sign up.',
+                style: TextStyle(
+                  color: Colors.green.shade900,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ]
+        )
+      ],
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(child: HeaderIcons()),
+                    Title(),
+                    SizedBox(height: 30),
+                    FormTextField(placeholder: 'Email'),
+                    SizedBox(height: 30),
+                    FormTextField(placeholder: 'Password'),
+                    SizedBox(height: 30),
+                    LoginOptions(),
+                    SizedBox(height: 30),
+                    LoginButton(),
+                    SizedBox(height: 30),
+                    LoginDivider(),
+                    SizedBox(height: 30),
+                    SocialMediaIcons(),
+                    Expanded(child: SignUpLink())
+                  ],
+                ),
+              ),
+            ]
+          ),
+        )
+      )
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.cyan.shade700,
+      ),
+      home: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("./lib/assets/login-bg.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
+        child: const MyHomePage(),
+      )
     );
   }
 }
